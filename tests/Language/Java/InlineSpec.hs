59eaae490256770678662fac342ee1e1715e064b
Mathieu Boespflug <m@tweag.io>
2016-12-26 10:27:29 +0100

    Adjust tests following merge of #14.

--- tests/Language/Java/InlineSpec.hs
+++ tests/Language/Java/InlineSpec.hs
@@ -6,6 +6,7 @@ module Language.Java.InlineSpec where
 
 import Data.Int
 import Foreign.JNI.Types (JObject)
+import Language.Java
 import Language.Java.Inline
 import Test.Hspec
 
@@ -13,31 +14,31 @@ spec :: Spec
 spec = do
     describe "Java quasiquoter" $ do
       it "Evaluates simple expressions" $ do
-        [java| 1 + 1 |] `shouldReturn` (2 :: Int32)
+        ([java| 1 + 1 |] >>= reify) `shouldReturn` (2 :: Int32)
 
       it "Evaluates simple blocks" $ do
-        [java| {
+        ([java| {
              int x = 1;
              int y = 2;
              return x + y;
-           } |] `shouldReturn` (3 :: Int32)
+           } |] >>= reify) `shouldReturn` (3 :: Int32)
 
       it "Supports antiquotation variables" $ do
         let x = 1 :: Int32
-        [java| $x + 1 |] `shouldReturn` (2 :: Int32)
+        ([java| $x + 1 |] >>= reify) `shouldReturn` (2 :: Int32)
 
       it "Supports multiple antiquotation variables" $ do
         let foo = 1 :: Int32
             bar = 2 :: Int32
-        [java| $foo + $bar |] `shouldReturn` (3 :: Int32)
+        ([java| $foo + $bar |] >>= reify) `shouldReturn` (3 :: Int32)
 
       it "Supports antiquotation variables in blocks" $ do
         let z = 1 :: Int32
-        [java| { return $z + 1; } |] `shouldReturn` (2 :: Int32)
+        ([java| { return $z + 1; } |] >>= reify) `shouldReturn` (2 :: Int32)
 
       it "Supports anonymous classes" $ do
         _ :: JObject <- [java| new Object() {} |]
         return ()
 
       it "Supports multiple anonymous classes" $ do
-        [java| new Object() {}.equals(new Object() {}) |] `shouldReturn` False
+        ([java| new Object() {}.equals(new Object() {}) |] >>= reify) `shouldReturn` False
