c4a8b3a7aa90a3cf8024efbeae794a76f365949a
Facundo Domínguez <facundo.dominguez@tweag.io>
2017-03-10 10:00:21 -0300

    inline-java: Forward arguments to JNI_CreateJavaVM in the classpath example.

--- examples/classpath/Main.hs
+++ examples/classpath/Main.hs
@@ -3,20 +3,24 @@
 module Main where
 
 import Data.Int (Int32)
+import Data.String (fromString)
 import Foreign.JNI (withJVM)
 import Language.Java.Inline
 import Language.Java.Inline.Cabal (gradleHooks)
+import System.Environment (getArgs)
 
 main :: IO Int32
-main = withJVM [] $ [java| {
-    org.apache.commons.collections4.OrderedMap map =
-      new org.apache.commons.collections4.map.LinkedMap();
-    map.put("FIVE", "5");
-    map.put("SIX", "6");
-    map.put("SEVEN", "7");
-    System.out.println(map.firstKey());
-    System.out.println(map.nextKey("FIVE"));
-    System.out.println(map.nextKey("SIX"));
-    return 0;
-    }
- |]
+main = do
+    args <- getArgs
+    withJVM (map fromString args) $ [java| {
+      org.apache.commons.collections4.OrderedMap map =
+        new org.apache.commons.collections4.map.LinkedMap();
+      map.put("FIVE", "5");
+      map.put("SIX", "6");
+      map.put("SEVEN", "7");
+      System.out.println(map.firstKey());
+      System.out.println(map.nextKey("FIVE"));
+      System.out.println(map.nextKey("SIX"));
+      return 0;
+      }
+   |]
