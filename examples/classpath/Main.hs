bd02b2418ac0fd03f075e65bdeb6268dff09d90f
Facundo Domínguez <facundo.dominguez@tweag.io>
2017-03-10 09:23:25 -0300

    Fix classpath example.

--- examples/classpath/Main.hs
+++ examples/classpath/Main.hs
@@ -2,11 +2,13 @@
 {-# LANGUAGE QuasiQuotes #-}
 module Main where
 
+import Data.Int (Int32)
+import Foreign.JNI (withJVM)
 import Language.Java.Inline
 import Language.Java.Inline.Cabal (gradleHooks)
 
-main :: IO ()
-main = [java| {
+main :: IO Int32
+main = withJVM [] $ [java| {
     org.apache.commons.collections4.OrderedMap map =
       new org.apache.commons.collections4.map.LinkedMap();
     map.put("FIVE", "5");
@@ -15,5 +17,6 @@ main = [java| {
     System.out.println(map.firstKey());
     System.out.println(map.nextKey("FIVE"));
     System.out.println(map.nextKey("SIX"));
+    return 0;
     }
  |]
