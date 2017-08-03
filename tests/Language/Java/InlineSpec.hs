5402aa238dedcf840d5483c0d45f9fc1c25f6ca6
Facundo Domínguez <facundo.dominguez@tweag.io>
2017-08-03 10:56:46 -0300

    Simplify tests when boxed values aren't used.

--- tests/Language/Java/InlineSpec.hs
+++ tests/Language/Java/InlineSpec.hs
@@ -31,18 +31,18 @@ spec = do
         [java| { } |] :: IO ()
 
       it "Evaluates simple expressions" $ do
-        ([java| 1 + 1 |] >>= reify) `shouldReturn` (2 :: Int32)
+        [java| 1 + 1 |] `shouldReturn` (2 :: Int32)
 
       it "Evaluates simple blocks" $ do
-        ([java| {
+        [java| {
              int x = 1;
              int y = 2;
              return x + y;
-           } |] >>= reify) `shouldReturn` (3 :: Int32)
+           } |] `shouldReturn` (3 :: Int32)
 
       it "Supports antiquotation variables" $ do
         let x = 1 :: Int32
-        ([java| $x + 1 |] >>= reify) `shouldReturn` (2 :: Int32)
+        [java| $x + 1 |] `shouldReturn` (2 :: Int32)
 
       describe "Type synonyms" $ do
         it "Supports top-level type synonym'ed antiquotation variables" $ do
@@ -71,7 +71,7 @@ spec = do
       it "Supports multiple antiquotation variables" $ do
         let foo = 1 :: Int32
             bar = 2 :: Int32
-        ([java| $foo + $bar |] >>= reify) `shouldReturn` (3 :: Int32)
+        [java| $foo + $bar |] `shouldReturn` (3 :: Int32)
 
       it "Supports repeated antiquotation variables" $ do
         obj :: JObject <- [java| new Object() {} |]
@@ -79,19 +79,18 @@ spec = do
 
       it "Supports antiquotation variables in blocks" $ do
         let z = 1 :: Int32
-        ([java| { return $z + 1; } |] >>= reify) `shouldReturn` (2 :: Int32)
+        [java| { return $z + 1; } |] `shouldReturn` (2 :: Int32)
 
       it "Supports anonymous classes" $ do
         _ :: JObject <- [java| new Object() {} |]
         return ()
 
       it "Supports multiple anonymous classes" $ do
-        ([java| new Object() {}.equals(new Object() {}) |] >>= reify) `shouldReturn` False
+        [java| new Object() {}.equals(new Object() {}) |] `shouldReturn` False
 
       it "Supports using antiquotation variables in inner classes" $ do
         let foo = 1 :: Int32
-        ([java| { class Foo { int f() { return $foo; } }; return 1; } |]
-          >>= reify) `shouldReturn` (1 :: Int32)
+        [java| { class Foo { int f() { return $foo; } }; } |] :: IO ()
 
       it "Supports import declarations" $ do
         -- Arrays comes from the java.util package.
