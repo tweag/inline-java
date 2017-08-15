b339799e013c1cd5b6c94cc63246ecdb4a82b502
Mathieu Boespflug <m@tweag.io>
2017-08-15 12:36:58 +0200

    C code style police.

--- cbits/bctable.h
+++ cbits/bctable.h
@@ -1,18 +1,18 @@
 /// A type of bytecodes
 struct inline_java_dot_class {
-    char* ij_dc_class_name;
-    int ij_dc_bc_count;
-    unsigned char* ij_dc_class_bytecode;
+	char *ij_dc_class_name;
+	int ij_dc_bc_count;
+	unsigned char *ij_dc_class_bytecode;
 };
 
 /// A type of linked lists for bytecode
 ///
 /// NULL is the empty linked list.
-struct inline_java_linked_list
-  { int ij_ll_dc_count;
-    struct inline_java_dot_class* ij_ll_dc;
-    struct inline_java_linked_list* ij_ll_next;
-  };
+struct inline_java_linked_list {
+	int ij_ll_dc_count;
+	struct inline_java_dot_class *ij_ll_dc;
+	struct inline_java_linked_list *ij_ll_next;
+};
 
 /// The bytecode table
 ///
@@ -20,10 +20,9 @@ struct inline_java_linked_list
 /// they are loaded.
 ///
 /// inline-java reads this table to load the classes in loadJavaWrappers.
-extern struct inline_java_linked_list* inline_java_bctable;
+extern struct inline_java_linked_list *inline_java_bctable;
 
 /// Adds an array of bytecodes at the front of the bctable.
 ///
 /// dc_count indicates the amount of bytecodes in the array.
-void inline_java_linked_list_cons(struct inline_java_dot_class* dc, int dc_count);
-
+void inline_java_linked_list_cons(struct inline_java_dot_class *dc, int dc_count);
