7e15dbb346106ea86801b249a3292155650bd677
Mathieu Boespflug <m@tweag.io>
2017-08-15 12:31:50 +0200

    Remove redundant init of global C variable.

--- cbits/bctable.c
+++ cbits/bctable.c
@@ -1,7 +1,7 @@
 #include <Rts.h>
 #include "bctable.h"
 
-struct inline_java_linked_list* inline_java_bctable = NULL;
+struct inline_java_linked_list* inline_java_bctable;
 
 void inline_java_linked_list_cons(struct inline_java_dot_class* dc, int dc_count) {
     struct inline_java_linked_list* n =
