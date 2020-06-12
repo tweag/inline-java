addef63b515a00fa1befe8278dde2a7b5f9a15a6
Facundo Domínguez <facundo.dominguez@tweag.io>
2020-06-12 12:56:35 +0000

    Update the linear-types-enabled ghc.

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/facundominguez/nixpkgs/archive/4e8e4d06055c97693b00cb3baba1d3660a905777.tar.gz")
+import (fetchTarball "https://github.com/tweag/nixpkgs/archive/6571b1ff07941d90745ce2d918de6e82d13c57bf.tar.gz")
