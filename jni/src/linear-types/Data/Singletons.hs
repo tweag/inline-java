420c6b58c5d637b88aee9dc9e3f1efc90ba5c1d0
Nicolas BACQUEY <nicolas.bacquey@tweag.io>
2020-07-03 17:28:00 +0200

    wip: fix jni (linear types enabled) to compile with GHC 8.11

--- jni/src/linear-types/Data/Singletons.hs
+++ jni/src/linear-types/Data/Singletons.hs
@@ -6,12 +6,13 @@
 {-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
 module Data.Singletons where
 
 import GHC.TypeLits (KnownSymbol, Symbol)
 
-data family Sing (ty :: k) :: *
+type family Sing (ty :: k) :: *
 
 class SingI (ty :: k)  where
   sing :: Sing ty
@@ -21,19 +22,23 @@ instance SingI ('[] :: [k]) where
 instance (SingI x, SingI xs) => SingI (x ': xs) where
   sing = SCons sing sing
 
-data instance Sing (n :: Symbol) = KnownSymbol n => SSym
+type instance Sing (n :: Symbol) = SSymbol n
+
+data SSymbol n = KnownSymbol n => SSym
 
 instance KnownSymbol n => SingI n where
   sing = SSym
 
-data instance Sing (a :: [b]) where
-  SNil :: Sing '[]
-  SCons :: Sing x -> Sing xs -> Sing (x ': xs)
+type instance Sing (a :: [b]) = SList a
+
+data SList xs where
+  SNil :: SList '[]
+  SCons :: Sing x -> SList xs -> SList (x ': xs)
 
 data SomeSing k where
   SomeSing :: Sing (a :: k) -> SomeSing k
 
-instance (forall x. Show (Sing (x :: k))) => Show (Sing (a :: [k])) where
+instance (forall x. Show (Sing (x :: k))) => Show (SList (a :: [k])) where
   showsPrec _ SNil = showString "SNil"
   showsPrec d (SCons ty tys) = showParen (d > 10) $
       showString "SCons " . showsPrec 11 ty . showChar ' ' . showsPrec 11 tys
