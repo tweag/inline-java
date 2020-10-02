be03a8b167732ef967208bc8968d20510eb98b5e
Nicolas BACQUEY <nicolas.bacquey@tweag.io>
2020-10-02 15:48:31 +0200

    doc: move documentation back to Foreign.JNI.Unsafe.hs

--- jni/src/common/Foreign/JNI/Unsafe/Internal.hs
+++ jni/src/common/Foreign/JNI/Unsafe/Internal.hs
@@ -1,23 +1,3 @@
--- | Low-level bindings to the Java Native Interface (JNI).
---
--- Read the
--- <https://docs.oracle.com/javase/8/docs/technotes/guides/jni/spec/jniTOC.html JNI spec>
--- for authoritative documentation as to what each of the functions in
--- this module does. The names of the bindings in this module were chosen to
--- match the names of the functions in the JNI spec.
---
--- All bindings in this module access the JNI via a thread-local variable of
--- type @JNIEnv *@. If the current OS thread has not yet been "attached" to the
--- JVM, it needs to be attached. See 'JNI.runInAttachedThread'.
---
--- The 'String' type in this module is the type of JNI strings. See
--- "Foreign.JNI.String".
---
--- The functions in this module are considered unsafe in opposition
--- to those in "Foreign.JNI.Safe", which ensure that local references are not
--- leaked.
---
-
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveAnyClass #-}
