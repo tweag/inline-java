69ca6b01e5504924b5205df920301df7e68fa626
Facundo Domínguez <facundo.dominguez@tweag.io>
2020-10-16 15:25:29 +0000

    Some style changes.

--- jni/src/common/Foreign/JNI/Unsafe.hs
+++ jni/src/common/Foreign/JNI/Unsafe.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
@@ -27,6 +28,7 @@
 -- Reexports definitions from "Foreign.JNI.Unsafe.Internal".
 module Foreign.JNI.Unsafe
   ( module Foreign.JNI.Unsafe.Internal
+  , NoSuchMethod(..)
   , getMethodID
   , getStaticMethodID
   ) where
@@ -36,33 +38,36 @@ import Data.Singletons (sing)
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
 import Foreign.JNI.Types
+import Foreign.JNI.Internal (MethodSignature(..))
 import qualified Foreign.JNI.String as JNI
 import qualified Foreign.JNI.Unsafe.Internal as Internal
 import Foreign.JNI.Unsafe.Internal hiding (getMethodID, getStaticMethodID)
 import Foreign.JNI.Unsafe.Internal.Introspection
 import System.IO.Unsafe (unsafePerformIO)
 
+-- | Throws when a method can't be found
 data NoSuchMethod = NoSuchMethod
-  { className :: Text.Text
-  , methodName :: Text.Text
-  , targetSignature :: Text.Text
-  , candidateSignatures :: [Text.Text]
+  { noSuchMethodClassName :: Text.Text
+  , noSuchMethodName ::  Text.Text
+  , noSuchMethodSignature :: Text.Text
+  , noSuchMethodCandidates :: [Text.Text]
   } deriving Exception
 
 instance Show NoSuchMethod where
-  show exn = Text.unpack $ Text.unwords $ case candidateSignatures exn of
-    [] ->
-      [ "No method named", methodName exn
-      , "was found in class", className exn
-      , ".\nWas there a mispelling ?"
-      ]
-    sigs ->
-      [ "No method named", methodName exn
-      , "has signature", targetSignature exn
-      , "in class", className exn
-      , ".\nCandidates methods are:"
-      , Text.unlines sigs
-      ]
+  show exn = Text.unpack $ Text.unwords $
+    case noSuchMethodCandidates exn of
+      [] ->
+        [ "No method named", noSuchMethodName exn
+        , "was found in class", noSuchMethodClassName exn
+        , ".\nWas there a mispelling ?"
+        ]
+      sigs ->
+        [ "No method named", noSuchMethodName exn
+        , "has signature", noSuchMethodSignature exn
+        , "in class", noSuchMethodClassName exn
+        , ".\nThe candidate methods are:"
+        , Text.unlines sigs
+        ]
 
 getMethodID
   :: JClass -- ^ A class object as returned by 'findClass'
@@ -99,11 +104,15 @@ handleJVMException
   -> MethodSignature
   -> JVMException
   -> IO a
-handleJVMException cls method sig (JVMException e) =
+handleJVMException cls method (MethodSignature sig) (JVMException e) =
   isInstanceOf (upcast e) kexception >>= \case
     True -> do
-      signatures <- getSignatures cls method
-      clsName <- getClassName cls
-      let methodNameTxt = Text.decodeUtf8 $ JNI.toByteString method
-      throwIO $ NoSuchMethod clsName methodNameTxt (Text.pack $ show sig) signatures
+      noSuchMethodCandidates <- getSignatures cls method
+      noSuchMethodClassName <- getClassName cls
+      throwIO $ NoSuchMethod
+        { noSuchMethodClassName
+        , noSuchMethodName = Text.decodeUtf8 (JNI.toByteString method)
+        , noSuchMethodSignature = Text.decodeUtf8 (JNI.toByteString sig)
+        , noSuchMethodCandidates
+        }
     False -> throwIO $ JVMException e
