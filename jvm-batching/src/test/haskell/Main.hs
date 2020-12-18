c0207bbd6c1529df1469a14fad9456906f29c3d5
Facundo Domínguez <facundo.dominguez@tweag.io>
2020-12-18 16:36:05 +0000

    Run tests in jvm-batching and jvm-streaming with bazel.

--- jvm-batching/src/test/haskell/Main.hs
+++ jvm-batching/src/test/haskell/Main.hs
@@ -6,13 +6,19 @@
 module Main where
 
 import Control.Concurrent (runInBoundThread)
+import Control.Exception (handle, throwIO)
+import qualified Data.Text.IO as Text
 import Foreign.JNI
 import Language.Java.Inline (loadJavaWrappers)
 import qualified Spec
 import Test.Hspec
+import System.IO (stderr)
 
 main :: IO ()
-main = withJVM ["-Djava.class.path=build/libs/jvm-batching.jar"] $ do
+main = withJVM ["-Djava.class.path=build/libs/jvm-batching.jar:jvm-batching/libjar.jar"] $ do
     loadJavaWrappers -- causes classes in jvm-batching.jar to be loaded
                      -- since they are used in java quotations
-    hspec $ around_ (runInBoundThread . runInAttachedThread) Spec.spec
+    hspec $ around_ (runInBoundThread . runInAttachedThread . printExceptions) Spec.spec
+  where
+    printExceptions = handle $ \e ->
+      showException e >>= Text.hPutStrLn stderr >> throwIO e
