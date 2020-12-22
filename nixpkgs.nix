1e6792414ed0f523fd856a6649544f0a8d523de9
Facundo Domínguez <facundo.dominguez@tweag.io>
2020-12-22 19:23:00 +0000

    Avoid bazel regression in nixpkgs for building in darwin

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,1 +1,5 @@
-import (fetchTarball "https://github.com/tweag/nixpkgs/archive/6411d7470f4d4f2fe8e67ced5b731b0e60d7e6a2.tar.gz")
+# Trying to workaround
+# https://github.com/NixOS/nixpkgs/issues/105573
+#
+# by going to a commit before the one introducing the regression.
+import (fetchTarball "https://github.com/nixos/nixpkgs/archive/a3a3dda3bacf61e8a39258a0ed9c924eeca8e293.tar.gz")
