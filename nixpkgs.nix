853a6c82ef1dd0ec6f3723b6d0d03a59de0ea3c9
Facundo Domínguez <facundo.dominguez@tweag.io>
2020-12-22 17:34:37 +0000

    Update nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/tweag/nixpkgs/archive/267bb1df046fbad76a64440994afdb5c0fd3bc99.tar.gz")
+import (fetchTarball "https://github.com/tweag/nixpkgs/archive/6411d7470f4d4f2fe8e67ced5b731b0e60d7e6a2.tar.gz")
