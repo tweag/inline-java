f5dd5ed641dfece05589450797272a1ac57950dc
Facundo Domínguez <facundo.dominguez@tweag.io>
2021-02-09 17:25:45 +0000

    Use fat jars with wizzardo-http-benchmark

--- benchmarks/wizzardo-http/src/main/haskell/Main.hs
+++ benchmarks/wizzardo-http/src/main/haskell/Main.hs
@@ -7,25 +7,22 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Main where
 
+import qualified Bazel.Runfiles as Runfiles
 import Control.Monad.IO.Class.Linear (MonadIO)
 import qualified Control.Functor.Linear as Linear
 import Data.Aeson
 import qualified Data.ByteString.Char8 as ByteString.Char8
 import Data.ByteString.Lazy (toStrict)
-import Data.List (intercalate)
-import Data.List.Split (splitOn)
 import Data.String (fromString)
 import qualified Data.Text as Text
 import DbHandler (createDbHandler)
 import Foreign.JNI.Safe (newGlobalRef_, withJVM, withLocalFrame_)
-import qualified Language.Haskell.TH.Syntax as TH
 import Language.Java.Inline.Safe
 import Language.Java.Safe (UnsafeUnrestrictedReference(..), reflect)
-import System.Directory (canonicalizePath)
-import System.Environment (getArgs, lookupEnv)
+import System.Environment (getArgs)
 import qualified System.IO.Linear as Linear
 import Wizzardo.Http.Handler (JHandler, createHandler)
-import Prelude (IO, Maybe(..), (<>), map, mapM, ($))
+import Prelude (IO, (<>), map, ($))
 import Prelude.Linear (Ur(..))
 
 imports "com.wizzardo.http.*"
@@ -34,17 +31,9 @@ imports "com.wizzardo.http.request.*"
 
 main :: IO ()
 main = do
-    let -- We use the classpath provided at build time.
-        cpArg = case $(do
-            cps <- TH.runIO $ do
-              Just cp <- lookupEnv "CLASSPATH"
-              -- We canonicalize the paths because the jars
-              -- are located under symbolic links that do not
-              -- survive the compilation.
-              mapM canonicalizePath $ splitOn ":" cp
-            TH.lift (intercalate ":" cps)
-          ) of
-          cp -> "-Djava.class.path=" <> fromString cp
+    r <- Runfiles.create
+    let jarPath = Runfiles.rlocation r "io_tweag_inline_java/benchmarks/wizzardo-http/jar_deploy.jar"
+        cpArg = "-Djava.class.path=" <> fromString jarPath
     args <- getArgs
     let -- We use the classpath provided at build time.
         otherJVMArgs =
