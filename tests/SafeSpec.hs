a46293d2e4fb9aa52837f3fe563882f1c6591cb4
Facundo Domínguez <facundo.dominguez@tweag.io>
2025-07-12 18:14:17 +0000

    Add back inline-java.cabal

--- tests/SafeSpec.hs
+++ tests/SafeSpec.hs
@@ -1,2 +1,2 @@
 {-# LANGUAGE CPP #-}
-{-# OPTIONS_GHC -F -pgmF HSPEC_DISCOVER -optF --module-name=SafeSpec #-}
+{-# OPTIONS_GHC -F -pgmF HSPEC_DISCOVER_HSPEC_DISCOVER_PATH -optF --module-name=SafeSpec #-}
